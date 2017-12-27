3d3f7634d026e2759b50cfbe3a4bea1dc5dbca03
Stefano Belli <sa.belli@hotmail.it>
2017-12-27 13:40:45 +0100

    print_memory_info and print_swap_info

--- src/print_memory_info.c
+++ src/print_memory_info.c
@@ -2,9 +2,42 @@
 #include "i3status.h"
 #include <yajl/yajl_gen.h>
 #include <yajl/yajl_version.h>
+#include <sys/sysinfo.h>
 
+#define GBYTE(xstat) ((((float)xstat) / 1048576) / 1000)
+
+//format: %avail, %total, %used
+//Gigabytes
 void print_memory_info(yajl_gen json_gen, char *buffer, const char* format) {
     char *outwalk = buffer;
 
-    //OUTPUT_FULL_TEXT("cant read load");
+#if defined(LINUX)
+    struct sysinfo current;
+    if(sysinfo(&current)) {
+        OUTPUT_FULL_TEXT("sysinfo() failure");
+        return;
+    }
+
+    for (; *format; format++) {
+        if (*format != '%') {
+            *(outwalk++) = *format;
+            continue;
+        }
+        
+        if (BEGINS_WITH(format + 1, "avail")) {
+            outwalk += sprintf(outwalk, "%3.2f", GBYTE(current.freeram));
+            format += 5;
+        } else if(BEGINS_WITH(format + 1, "total")) {
+            outwalk += sprintf(outwalk, "%3.2f", GBYTE(current.totalram));
+            format += 5;
+        }  else if(BEGINS_WITH(format + 1, "used")) {
+            outwalk += sprintf(outwalk, "%3.2f", GBYTE(current.totalram - current.freeram));
+            format += 4;
+        } 
+    }
+
+    OUTPUT_FULL_TEXT(buffer);
+#else
+    OUTPUT_FULL_TEXT("missing implementation");
+#endif
 }
